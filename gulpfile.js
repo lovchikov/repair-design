73931096eb66281d5c0ba3f08ffb961932983b66
Constantine-Reflex <aztecmaster@mail.ru>
2020-03-15 06:14:08 +0300

    feat: add Futura Book & Demi Fonts

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,12 @@ function bs() {
     });
     watch("./*.html").on('change', browserSync.reload);
     watch("./sass/**/*.sass", serveSass);
+    watch("./sass/**/*.scss", serveSass);
     watch("./js/*.html").on('change', browserSync.reload);
 };
 
 function serveSass() {
-    return src("./sass/**/*.sass, ./sass/**/*.scss")
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
         .pipe(sass())
         .pipe(autoprefixer({
             cascade: false
