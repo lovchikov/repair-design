468bb1a8b9f4903f87d765df8dffd3a264fea4bc
Constantine-Reflex <aztecmaster@mail.ru>
2020-03-25 18:41:16 +0300

    feat: add validation to form

--- js/main.js
+++ js/main.js
@@ -48,4 +48,37 @@ $(document).ready(function () {
 
         new WOW().init();
 
+        // Валидация формы
+        $('.modal__form').validate({
+            errorClass: "invalid",
+            rules: {
+                // строчное правило
+                userName: {
+                    required: true,
+                    minlength: 2
+                },
+                userPhone: "required",
+                // правило-объект (блок)
+                userEmail: {
+                  required: true,
+                  email: true
+                }
+            }, // Сообщения
+            messages: {
+                userName: {
+                    required: "Имя обязательно",
+                    minlength: "Имя не короче двух букв"
+                },
+                userPhone: "Телефон обязателен",
+                userEmail: {
+                    required: "Обязательно укажите email",
+                    email: "Введите в формате: name@domain.com"
+                }
+            }
+        });
+
+        // Маска для номера телефона
+
+        $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7(___) __-__-___"});
+
 });
